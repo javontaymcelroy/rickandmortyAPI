6bce2f82e1aeae466f7e3a03ad216478912ea93e
Javontay McElroy <dinomightnetwork@gmail.com>
2019-05-18 14:10:01 -0500

    Stylingggggggg

--- src/App.js
+++ src/App.js
@@ -9,68 +9,94 @@ import Home from './components/Home';
 import Character from './components/Character';
 import About from './components/About';
 
+import bg from './assets/rickandmortybg.png';
 
-class App extends React.Component{
-  constructor(){
+class App extends React.Component {
+  constructor() {
     super();
     this.state = {
       data: [],
-      pageNum: 2
-    }
+      pageNum: 1
+    };
   }
 
-  componentDidMount(){
+  componentDidMount() {
     axios
-    .get(`https://rickandmortyapi.com/api/character/?page=${this.state.pageNum}`)
-    .then( res => {
-      this.setState({ data: res.data.results })
-    })
-    .catch( err => {
-      console.log(err)
-    })
+      .get(
+        `https://rickandmortyapi.com/api/character/?page=${this.state.pageNum}`
+      )
+      .then(res => {
+        this.setState({ data: res.data.results });
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
 
-  pageChange = e => {
+  componentDidUpdate() {
+    axios
+      .get(
+        `https://rickandmortyapi.com/api/character/?page=${this.state.pageNum}`
+      )
+      .then(res => {
+        this.setState({ data: res.data.results });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  pageChangeDecrement = e => {
     e.preventDefault();
-    console.log('clicked')
-    if(this.state.pageNum === 1) return;
-    this.setState({ 
+    if (this.state.pageNum === 1) return;
+    this.setState({
+      pageNum: --this.state.pageNum
+    });
+  };
+
+  pageChangeIncrement = e => {
+    e.preventDefault();
+    this.setState({
       pageNum: ++this.state.pageNum
-     })
-     console.log(this.state.pageNum);
-  }
+    });
+  };
 
-
-  render(){
+  render() {
     return (
-      <div className="App">
-
-        <nav className="nav-links">
-          <NavLink exact to="/">Home</NavLink>{" "}
-          <NavLink exact to="/character-list">Characters</NavLink>
-          <NavLink exact to="/about">About</NavLink>
-        </nav>
-
-        <Route exact path="/" component={Home} />
-        <Route 
-        exact path="/character-list" 
-        render={ props => 
-        <CharacterList 
-        {...props} 
-        data={this.state.data} 
-        pageNum={this.state.pagNum}
-        pageChange={this.pageChange}
-        /> } />
-        <Route 
-        path="/character-list/:id"
-        render={ props => 
-        <Character 
-        {...props}
-        data={this.state.data} 
-        /> }
-        /> 
-        <Route path="/about" component={About} />
-      </div>
+      <>
+        <div className='App'>
+          <nav className='nav-links'>
+            <NavLink exact to='/'>
+              Home
+            </NavLink>{' '}
+            <NavLink exact to='/character-list'>
+              Characters
+            </NavLink>
+            <NavLink exact to='/about'>
+              About
+            </NavLink>
+          </nav>
+          <Route exact path='/' component={Home} />
+          <Route
+            exact
+            path='/character-list'
+            render={props => (
+              <CharacterList
+                {...props}
+                data={this.state.data}
+                pageNum={this.state.pagNum}
+                pageChange={this.pageChange}
+              />
+            )}
+          />
+          <Route
+            path='/character-list/:id'
+            render={props => <Character {...props} data={this.state.data} />}
+          />
+          <Route path='/about' component={About} />
+        </div>
+        <img src={bg} alt='bg' className='background' />
+      </>
     );
   }
 }
